82c4a306eda623632ea6637c04cabba24868249e
Dnimf <danielmf1@al.insper.edu.br>
2024-05-08 17:13:22 -0300

    primeira tentativa

--- aaaaaaaaa.py
+++ aaaaaaaaa.py
@@ -1,2 +1,66 @@
 print("tenho f")
 print("obrigado")
+import pygame
+import random
+
+pygame.init()
+background=(0,0,225)
+# ----- Gera tela principal
+WIDTH = 480
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+METEOR_WIDTH = 50
+METEOR_HEIGHT = 38
+SHIP_WIDTH = 50
+SHIP_HEIGHT = 38
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load(r'C:\Users\Daniel\referencia\assets\img\starfield.png').convert()
+meteor_img = pygame.image.load(r'').convert_alpha()
+meteor_img = pygame.transform.scale(meteor_img, (METEOR_WIDTH, METEOR_HEIGHT))
+class inimigo(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 5
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+all_sprites=pygame.sprite.Group()
+slime=inimigo(meteor_img)
+all_sprites.add(slime)
+game=True
+clock = pygame.time.Clock()
+FPS = 30
+while game:
+    clock.tick(FPS)
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+                game = False
+
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição dos meteoros
+    all_sprites.update()
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    # Desenhando meteoros
+    all_sprites.draw(window)
+
+pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit() 
+
